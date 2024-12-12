7bd402f0a99f774cbdb62e979690872db9dd2c71
Stepper <true.ste.xin@gmail.com>
2024-12-12 09:46:11 +0100

    Version 0.0.1 - Details: Added Version Controller

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XinWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        public VersionController() { }
+
+        [HttpGet]
+        public async Task<IActionResult> getVersion()
+        {
+            string versionNumber = "0.0.1";
+            string versionDescription = "Added Version Controller";
+            string currVersion = $"Version {versionNumber} - Details: {versionDescription}";
+            return Ok(currVersion);
+
+        }
+    }
+}
